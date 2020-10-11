e4c46d9bf94f5d1fba557ddb179fbd324940fc3a
Florian <florian0205@gmx.de>
2020-10-11 22:59:46 +0200

    MainMenuUpdate1

---
+++ Assets/Scripts/MainMenu/ButtonTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public GameObject box;
+    public Material m1;
+    public Material m2;
+
+    public void OnDown()
+    {
+        box.GetComponent<MeshRenderer>().material = m1;
+    }
+
+    public void OnUp()
+    {
+        box.GetComponent<MeshRenderer>().material = m2;
+    }
+}

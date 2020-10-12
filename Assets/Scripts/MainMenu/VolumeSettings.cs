bc73d975fd707459203682d9ccd0a072eb3e4f3d
Florian <florian0205@gmx.de>
2020-10-12 14:26:20 +0200

    VolumeTest

---
+++ Assets/Scripts/MainMenu/VolumeSettings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSettings : MonoBehaviour
+{
+    public Transform knob;
+    public Transform start;
+    public Transform end;
+    public float volume;
+
+    private Vector3 lastPos;
+    private float abstand;
+    private float einProzent;
+
+    private void Start()
+    {
+        lastPos = knob.position;
+        abstand = distance(end.position, start.position);
+        einProzent = abstand / 100;
+    }
+
+    void FixedUpdate()
+    {
+        if (knob.position != lastPos)
+        {
+            lastPos = knob.position;
+            float distanceToStart = distance(knob.position, start.position);
+            volume = distanceToStart / einProzent;
+            Debug.Log(volume);
+        }
+    }
+
+    float distance(Vector3 a, Vector3 b)
+    {
+        Vector3 VerbindungsVector = b - a;
+        double x = Math.Pow(VerbindungsVector.x, 2);
+        double y = Math.Pow(VerbindungsVector.y, 2);
+        double z = Math.Pow(VerbindungsVector.z, 2);
+        float distance = (float)Math.Sqrt(x + y + z);
+        return distance;
+    }
+}

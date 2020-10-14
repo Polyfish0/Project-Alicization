d5730e3e03700049fd932be16b969c044bf508df
Florian <florian0205@gmx.de>
2020-10-14 13:26:31 +0200

    VolumeFinished | SettingsTable ready for testing

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,10 @@
+﻿enum Scenes
+{
+    MainMenu = 0,
+    SampleScene = 1
+}
+
+enum Items
+{
+
+}
---
+++ Assets/Scripts/MainMenu/SettingsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public GameObject volumeSettings;
+    public GameObject debugButton2;
+
+    private List<GameObject> allSettings;
+
+    private void Start()
+    {
+        allSettings.Add(volumeSettings);
+    }
+
+    private void activateSetting(GameObject settingToActivate)
+    {
+        foreach (GameObject gameObject in allSettings)
+        {
+            if (gameObject != settingToActivate)
+            {
+                gameObject.SetActive(false);
+            }
+            settingToActivate.SetActive(true);
+        }
+    }
+
+    public void onVolumeDown()
+    {
+        activateSetting(volumeSettings);
+    }
+
+    public void onDebugTwo()
+    {
+        activateSetting(debugButton2);
+    }
+}
